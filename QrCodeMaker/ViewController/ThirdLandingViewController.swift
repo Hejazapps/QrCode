d3964b9170c7c030c87a080ac08e15fa55cb1c37
Hejaz apps <hejazapps@gmail.com>
2024-02-17 22:34:08 +0600

    Updated 1st page

--- QrCodeMaker/ViewController/ThirdLandingViewController.swift
+++ QrCodeMaker/ViewController/ThirdLandingViewController.swift
@@ -8,22 +8,15 @@
 import UIKit
 
 class ThirdLandingViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func swiped(_ sender: UISwipeGestureRecognizer) {
+        if sender.direction == .right {
+            self.navigationController?.popViewController(animated: true)
+        } else {
+            print("swipe direction: \(sender.direction)")
+        }
     }
-    */
-
 }
